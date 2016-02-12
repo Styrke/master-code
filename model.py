a104cbed201a55dbb496340297a8a06d8c3ccb08
Jonas Meinertz Hansen <jonas@meinertz.org>
2016-02-12 21:36:22 +0100

    Add *very* simple recurrent layer
    Each time step just updates the output to be equal to the current input,
    so the output will always be the last input.
    
    Note that this model uses TensorArrays, which are only availave in
    _very_ recent TF builds from the last few weeks.

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.python.ops.tensor_array_ops import TensorArray
+from tensorflow.python.ops import control_flow_ops
 
 EMBEDDING_SIZE = 3
 
@@ -8,6 +10,23 @@ def inference(alphabet_size, input):
     embeddings = tf.random_uniform(shape=[alphabet_size, EMBEDDING_SIZE])
     embedded = tf.gather(embeddings, input)
 
-    shp = tf.shape(embedded)
+    size = tf.shape(embedded)[1] # number of loop iterations
 
-    return embedded
+    # Define loop condition and loop body
+    def cond(i, inputs, output):
+        return tf.less(i, size)
+
+    def body(i, inputs, output):
+        output = inputs.read(i)
+        return [tf.add(i, 1), inputs, output]
+
+    # Initialize loop variables
+    i = tf.constant(1)
+    # transpose so the TensorArray divides the tensor by the time dimension
+    transp = tf.transpose(embedded, perm=[1, 0, 2])
+    inputs = TensorArray(tf.float32, size).unpack(transp)
+    output = tf.constant(1.)
+
+    [i, inputs, output] = control_flow_ops.While(cond, body, [i, inputs, output])
+
+    return output
