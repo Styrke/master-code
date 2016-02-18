ed5adfbd9152d3c4fc3a5589f1943cd422b58f92
Elias Obeid <e.k.obeid@gmail.com>
2016-02-18 15:37:56 +0100

    added summary operations to capture scalar summaries

--- train.py
+++ train.py
@@ -9,23 +9,30 @@ with tf.Session() as sess:
     lengths = tf.placeholder(tf.int32)
     ts = tf.placeholder(tf.int64)
     out = tf.squeeze(model.inference(alphabet_size=15, input=input, lengths=lengths))
-    loss = model.loss(out, ts)
 
+    with tf.name_scope('xent'):
+      loss = model.loss(out, ts)
+      _ = tf.scalar_summary('loss', tf.reduce_mean(loss))
     # initialize parameters
     tf.initialize_all_variables().run()
 
-    # copute accuracy
-    acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), ts), dtype=tf.float32))
+    with tf.name_scope('accuracy'):
+      # compute accuracy
+      acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), ts), dtype=tf.float32))
+      _ = tf.scalar_summary('accuracy', acc)
+
 
     # optimize
     optimizer = tf.train.GradientDescentOptimizer(0.01)
     optimizer_op = optimizer.minimize(loss)
 
-    inp = np.array([[1, 0, 2],
-                    [2, 0, 1]])
-    lens = [2, 3]
+    # merge summaries and add writer
+    merged = tf.merge_all_summaries()
+    writer = tf.train.SummaryWriter('/tmp/test_logs', sess.graph_def)
+
     for i in xrange(100):
         targets, inputs, masks, lens = get_batch(32)
-        res = sess.run([loss, acc, optimizer_op], feed_dict={input: inputs, lengths: lens, ts: targets})
+        res = sess.run([loss, acc, optimizer_op, merged], feed_dict={input: inputs, lengths: lens, ts: targets})
         if i % 10 == 0:
             print res[1], np.mean(res[0])
+        writer.add_summary(res[3], i)
