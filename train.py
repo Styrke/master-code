de1ce20316442d2e0bf504c9adbb8e744835f2d0
Elias Obeid <e.k.obeid@gmail.com>
2016-02-18 15:49:57 +0100

    updated variable names for train.py

--- train.py
+++ train.py
@@ -7,11 +7,11 @@ from gen_dummy_data import get_batch
 with tf.Session() as sess:
     input = tf.placeholder(tf.int32, name='input')
     lengths = tf.placeholder(tf.int32, name='lengths')
-    ts = tf.placeholder(tf.int64, name='targets')
+    labels = tf.placeholder(tf.int64, name='labels')
     out = tf.squeeze(model.inference(alphabet_size=15, input=input, lengths=lengths), name='output')
 
     with tf.name_scope('xent'):
-      loss = model.loss(out, ts)
+      loss = model.loss(out, labels)
       _ = tf.scalar_summary('loss', tf.reduce_mean(loss))
 
     # initialize parameters
@@ -19,20 +19,19 @@ with tf.Session() as sess:
 
     with tf.name_scope('correct_predictions'):
       # compute accuracy
-      acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), ts), dtype=tf.float32), name='prediction_accuracy')
-      _ = tf.scalar_summary('accuracy', acc)
+      accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), labels), dtype=tf.float32), name='prediction_accuracy')
+      _ = tf.scalar_summary('accuracy', accuracy)
 
     # optimize
-    optimizer = tf.train.GradientDescentOptimizer(0.01)
-    optimizer_op = optimizer.minimize(loss)
+    optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
     # merge summaries and add writer
     merged = tf.merge_all_summaries()
     writer = tf.train.SummaryWriter('/tmp/test_logs', sess.graph_def)
 
     for i in xrange(100):
-        targets, inputs, masks, lens = get_batch(32)
-        res = sess.run([loss, acc, optimizer_op, merged], feed_dict={input: inputs, lengths: lens, ts: targets})
+        targets, inputs, _, lens = get_batch(32)
+        res = sess.run([loss, accuracy, optimizer, merged], feed_dict={input: inputs, lengths: lens, labels: targets})
         if i % 10 == 0:
             print res[1], np.mean(res[0])
         writer.add_summary(res[3], i)
