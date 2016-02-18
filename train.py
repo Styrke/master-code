3226f4a3ad6321c5e9f7dea913ead3ef0f23a39d
Elias Obeid <e.k.obeid@gmail.com>
2016-02-18 15:43:15 +0100

    added names to train.py

--- train.py
+++ train.py
@@ -5,22 +5,22 @@ import model
 from gen_dummy_data import get_batch
 
 with tf.Session() as sess:
-    input = tf.placeholder(tf.int32)
-    lengths = tf.placeholder(tf.int32)
-    ts = tf.placeholder(tf.int64)
-    out = tf.squeeze(model.inference(alphabet_size=15, input=input, lengths=lengths))
+    input = tf.placeholder(tf.int32, name='input')
+    lengths = tf.placeholder(tf.int32, name='lengths')
+    ts = tf.placeholder(tf.int64, name='targets')
+    out = tf.squeeze(model.inference(alphabet_size=15, input=input, lengths=lengths), name='output')
 
     with tf.name_scope('xent'):
       loss = model.loss(out, ts)
       _ = tf.scalar_summary('loss', tf.reduce_mean(loss))
+
     # initialize parameters
     tf.initialize_all_variables().run()
 
-    with tf.name_scope('accuracy'):
+    with tf.name_scope('correct_predictions'):
       # compute accuracy
-      acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), ts), dtype=tf.float32))
+      acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), ts), dtype=tf.float32), name='prediction_accuracy')
       _ = tf.scalar_summary('accuracy', acc)
-
 
     # optimize
     optimizer = tf.train.GradientDescentOptimizer(0.01)
