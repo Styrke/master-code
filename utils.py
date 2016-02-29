f28263ada3677ab40c6b417618f15919de000aa2
Elias Obeid <e.k.obeid@gmail.com>
2016-02-29 13:15:11 +0100

    added reverse dictionary function

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 ## A collection of methods used in the master-code repository
 
+def reverse_dict(d):
+    return { v: k for (k, v) in d.iteritems() }
+
+
 def dict_to_char(alphadict, int_list_in):
     char_list_out
     for int_in in int_list_in:
