8203c6fed90a3e19dae612b2768c90494ce2bb14
A. Rosenberg Johansen <alexander@herhjemme.dk>
2016-02-29 12:42:30 +0100

    from int to char

---
+++ utils.py
@@ -0,0 +1,9 @@
+## A collection of methods used in the master-code repository
+
+def dict_to_char(alphadict, int_list_in):
+    char_list_out
+    for int_in in int_list_in:
+        for char_dict, int_dict in alphadict.items():
+            if int_dict == int_in:
+                char_list_out.append(char_dict)
+    return char_list_out
