39b1f1b238b6f07a6013a9e59740fd5d682cbca1
Jonas Meinertz Hansen <jonas@meinertz.org>
2016-03-04 22:14:43 +0100

    Add script for downloading europarl dataset
    Quick and easy

---
+++ data/get_europarl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L https://www.dropbox.com/s/ncc3ekjwts1hw8p/europarl-v7.fr-en.en?dl=1 https://www.dropbox.com/s/wgoqocs3jdgphzv/europarl-v7.fr-en.fr?dl=1 -o train/europarl-v7.fr-en.en -o train/europarl-v7.fr-en.fr
