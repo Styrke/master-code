442657421c2157e2edd60ab869b931a00a8a962a
Elias Obeid <e.k.obeid@gmail.com>
2016-03-16 12:49:42 +0100

    removed -f from command

--- setup/docker/docker-build-tag.sh
+++ setup/docker/docker-build-tag.sh
@@ -4,4 +4,4 @@ IMAGE=obeyed/py3-tf-gpu
 ID=$(docker build  -t ${IMAGE} . | tail -1 | sed 's/.*Successfully built \(.*\)$/\1/')
 
 docker tag ${ID} ${IMAGE}:${VERSION}
-docker tag -f ${ID} ${IMAGE}:latest
+docker tag ${ID} ${IMAGE}:latest
