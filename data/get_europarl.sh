e13bb056d03ac222dcac2c69699cd8d19b59f0c8
Jonas Meinertz Hansen <jonas@meinertz.org>
2016-04-06 11:34:33 +0200

    Update data downloader script

--- data/get_europarl.sh
+++ data/get_europarl.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-mkdir train/ && curl -L https://www.dropbox.com/s/ncc3ekjwts1hw8p/europarl-v7.fr-en.en?dl=1 https://www.dropbox.com/s/wgoqocs3jdgphzv/europarl-v7.fr-en.fr?dl=1 -o train/europarl-v7.fr-en.en -o train/europarl-v7.fr-en.fr
+curl -# --create-dirs -L \
+  https://www.dropbox.com/s/ncc3ekjwts1hw8p/europarl-v7.fr-en.en?dl=1 \
+  -o train/europarl-v7.fr-en.en \
+  https://www.dropbox.com/s/wgoqocs3jdgphzv/europarl-v7.fr-en.fr?dl=1 \
+  -o train/europarl-v7.fr-en.fr \
+  https://www.dropbox.com/s/17rqqouwxoa1l98/europarl-v7.da-en.en?dl=1 \
+  -o ./train/europarl-v7.da-en.en \
+  https://www.dropbox.com/s/rltf6dipngwlw6e/europarl-v7.da-en.da?dl=1 \
+  -o ./train/europarl-v7.da-en.da \
