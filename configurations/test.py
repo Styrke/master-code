3208011f6b63833ae0244a8aad0effef37274699
Jonas Meinertz Hansen <jonas@meinertz.org>
2016-04-07 15:49:04 +0200

    Add lightweight debugging configuration as default

--- configurations/test.py
+++ configurations/test.py
@@ -4,5 +4,16 @@ import model
 
 
 class Model(model.Model):
+    """Debugging configuration.
+
+    This is the default configuration that is used when the training
+    script is launched without naming a specific configuration file.
+
+    This configuration uses the default model architecture but uses
+    very small hyperparameters to enable quick debugging of training
+    script and model architecture.
+    """
     # overwrite config
     batch_size = 32
+    seq_len = 25
+    rnn_units = 100
