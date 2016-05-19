101e853202b4f848300a09fac2001a4d2c5ef429
A. Rosenberg Johansen <alexander@herhjemme.dk>
2016-05-19 15:56:40 +0200

    higher memory usage

--- train.py
+++ train.py
@@ -97,7 +97,7 @@ class Trainer:
 
     def train(self):
         print("Training..")
-        gpu_opts = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)
+        gpu_opts = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
         with tf.Session(config=tf.ConfigProto(gpu_options=gpu_opts)) as sess:
             # Prepare for writing TensorBoard summaries
             if self.tb_log_freq and self.name:
