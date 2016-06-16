b69e8c707bb4330e7f6679d3677bb2d3b3898923
Alexander Rosenberg Johansen <alexander@herhjemme.dk>
2016-06-16 09:01:02 +0000

    wmt train data

--- data/get_wmt_deen.sh
+++ data/get_wmt_deen.sh
@@ -2,13 +2,13 @@
 
 echo "Downloading training datasets"
 
-#curl -# --create-dirs -L \
-#  http://www.statmt.org/wmt13/training-parallel-europarl-v7.tgz \
-#  -o data/train/training-parallel-europarl-v7.tgz \
-#  http://www.statmt.org/wmt13/training-parallel-commoncrawl.tgz \
-#  -o data/train/training-parallel-commoncrawl.tgz \
-#  http://www.statmt.org/wmt15/training-parallel-nc-v10.tgz \
-#  -o data/train/training-parallel-nc-v10.tgz \
+curl -# --create-dirs -L \
+  http://www.statmt.org/wmt13/training-parallel-europarl-v7.tgz \
+  -o data/train/training-parallel-europarl-v7.tgz \
+  http://www.statmt.org/wmt13/training-parallel-commoncrawl.tgz \
+  -o data/train/training-parallel-commoncrawl.tgz \
+  http://www.statmt.org/wmt15/training-parallel-nc-v10.tgz \
+  -o data/train/training-parallel-nc-v10.tgz \
 
 curl -# --create-dirs -L \
   http://www.statmt.org/wmt15/dev-v2.tgz \
