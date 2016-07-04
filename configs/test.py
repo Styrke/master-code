02c70550abe7a268f6a731fbac8e6e9bb1056cf9
alexander <alexander@herhjemme.dk>
2016-07-04 08:26:47 +0000

    config/test

--- configs/test.py
+++ configs/test.py
@@ -15,5 +15,3 @@ class Model(default.Model):
     valid_freq = 100  # Using a smaller valid set for debug so can do it more frequently.
 
     # only use a single of the validation files for this debugging config
-    valid_x_files = ['data/valid/devtest2006.en']
-    valid_t_files = ['data/valid/devtest2006.da']
