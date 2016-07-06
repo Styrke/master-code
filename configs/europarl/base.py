1f234d1e0b741dc29b688bd7af91b73684e08180
alexander <alexander@herhjemme.dk>
2016-07-06 11:16:49 +0000

    europarl batch size and valid freq

--- configs/europarl/base.py
+++ configs/europarl/base.py
@@ -3,7 +3,6 @@ from data.alphabet import Alphabet
 
 class Model(default.Model):
     # overwrite config
-    batch_size = 128
     seq_len_x = 250
     seq_len_t = 500
-    valid_freq = 3000
+    valid_freq = 1000
