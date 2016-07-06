3fcea320958fde731476365a6d1500e358ba812a
Alexander R. Johansen <alexander@herhjemme.dk>
2016-07-06 11:51:34 +0000

    c2w-fren

--- configs/europarl/c2w-fren.py
+++ configs/europarl/c2w-fren.py
@@ -5,13 +5,13 @@ class Model(c2w.Model):
     # overwrite config
     name = 'europarl/c2w-fren'
     # datasets
-    train_x_files = ['data/train/europarl-v7.de-en.fr']
-    train_t_files = ['data/train/europarl-v7.de-en.en']
+    train_x_files = ['data/train/europarl-v7.fr-en.fr']
+    train_t_files = ['data/train/europarl-v7.fr-en.en']
     valid_x_files = ['data/valid/devtest2006.fr']
     valid_t_files = ['data/valid/devtest2006.en']
     test_x_files = ['data/valid/test2007.fr']
     test_t_files = ['data/valid/test2007.en']
 
     # settings that are local to the model
-    alphabet_src = Alphabet('data/alphabet/dict_europarl.de-en.fr', eos='*')
-    alphabet_tar = Alphabet('data/alphabet/dict_europarl.de-en.en', eos='*', sos='')
+    alphabet_src = Alphabet('data/alphabet/dict_europarl.fr-en.fr', eos='*')
+    alphabet_tar = Alphabet('data/alphabet/dict_europarl.fr-en.en', eos='*', sos='')
