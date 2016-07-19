a978dd08a61f47c60163955284c3dd4619f8da2a
Alexander Rosenberg Johansen <alexander@herhjemme.dk>
2016-07-19 18:16:10 +0000

    correct placement and names of wmt config files

--- configs/char_to_char/wmt-ende.py
+++ configs/char_to_char/wmt-ende.py
@@ -4,17 +4,3 @@ from data.alphabet import Alphabet
 class Model(base.Model):
     # overwrite config
     name = 'char_to_char/wmt-ende'
-    train_x_files = ['data/train/europarl-v7.de-en.de.tok',
-                     'data/train/commoncrawl.de-en.de.tok',
-                     'data/train/news-commentary-v10.de-en.de.tok']
-    train_t_files = ['data/train/europarl-v7.de-en.en.tok',
-                     'data/train/commoncrawl.de-en.en.tok',
-                     'data/train/news-commentary-v10.de-en.en.tok']
-    valid_x_files = ['data/valid/newstest2013.de.tok']
-    valid_t_files = ['data/valid/newstest2013.en.tok']
-    test_x_files = ['data/valid/newstest2014.deen.de.tok']
-    test_t_files = ['data/valid/newstest2014.deen.en.tok']
-
-    # settings that are local to the model
-    alphabet_src = Alphabet('data/alphabet/dict_wmt_tok.de-en.de', eos='*') 
-    alphabet_tar = Alphabet('data/alphabet/dict_wmt_tok.de-en.en', eos='*', sos='')
